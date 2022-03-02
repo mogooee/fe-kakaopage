6c1828383cf2cf0030d8b6f3f8389017902be9b3
연 <mogoo22@naver.com>
2022-03-02 14:09:23 +0900

    fix: json fetch 경로를 깃허브 링크로 변경

--- public/js/rendering/main-ad-banner.js
+++ public/js/rendering/main-ad-banner.js
@@ -3,7 +3,9 @@ import { mainAdBanner } from "../components/main-ad-banner.js";
 import { makingClone, changeTab } from "../event/event-slide.js";
 
 export const renderingMainAd = (selectedValue) => {
-  const item = fetch("../../../data/ad-data.json")
+  const item = fetch(
+    "https://raw.githubusercontent.com/mogooee/fe-kakaopage/demo/data/ad-data.json"
+  )
     .then((response) => response.json())
     .then((json) => json.filter((e) => e.category === selectedValue))
     .then((filter) =>
